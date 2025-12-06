46a6d3ce64973f86f9c19a9e10d6cfea18734762
Denis Eroshevskii <devskjj@gmail.com>
2025-12-06 11:28:53 +0600

    "Подготовил структуру, папки и стандартные классы."

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
+import models.DataModel;
+import server.ServerLogic;
 
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            DataModel dataModel = new DataModel();
+            new ServerLogic("localhost", 9889, dataModel).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/server/interfaces/RouteHandler.java
@@ -0,0 +1,8 @@
+package server.interfaces;
+
+import com.sun.net.httpserver.HttpExchange;
+
+@FunctionalInterface
+public interface RouteHandler {
+    void handle(HttpExchange exchange);
+}
